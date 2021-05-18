f579f5be1f80bf30cb501422c57ce014c6a74dee
Droze <84060938+Drozeee@users.noreply.github.com>
2021-05-17 22:14:25 -0400

    Add files via upload

---
+++ remove xbox bloat.bat
@@ -0,0 +1,30 @@
+ÿþ
+@echo off
+color 2
+
+echo Made by Droze!
+
+echo Type "yes" to uninstall xbox bloatware and "no" to cancel
+set /p con=
+if /i "%con%" == "yes" goto :yes
+if /i "%con%" == "no" goto :no
+
+:yes 
+powershell -command "Get-AppxPackage Microsoft.GamingServices | Remove-AppxPackage"
+powershell -command "Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage"
+powershell -command "Get-AppxPackage Microsoft.XboxGameOverlay | Remove-AppxPackage"
+powershell -command "Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage"
+powershell -command "Get-AppxPackage Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage"
+powershell -command "Get-AppxPackage Microsoft.XboxGameCallableUI | Remove-AppxPackage"
+powershell -command "Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage"
+
+exit
+
+
+
+
+
+
+:no
+
+exit
